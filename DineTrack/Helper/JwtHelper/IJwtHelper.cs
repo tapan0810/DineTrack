ee523d3655c072545e7133a3f321ad38f83f5ef4
tapan0810 <arceus0810@gmail.com>
2026-02-25 19:20:28 +0530

    feat: Implement Helper class for JwtHelper for Generating Token

---
+++ DineTrack/Helper/JwtHelper/IJwtHelper.cs
@@ -0,0 +1,9 @@
+﻿using DineTrack.Entities.Models;
+
+namespace DineTrack.Helper.JwtHelper
+{
+    public interface IJwtHelper
+    {
+        public string GenerateJwtToken(User user);
+    }
+}
---
+++ DineTrack/Helper/JwtHelper/JwtHelper.cs
@@ -0,0 +1,37 @@
+﻿using DineTrack.Entities.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace DineTrack.Helper.JwtHelper
+{
+    public class JwtHelper(IConfiguration configuration) : IJwtHelper
+    {
+        public string GenerateJwtToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.FullName),
+                new Claim(ClaimTypes.NameIdentifier,user.UserId.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["AppSettings:Token"]!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var toeknDescriptor = new JwtSecurityToken(
+                
+                issuer: configuration.GetValue<string>("AppSettings:issuer"),
+                audience:configuration.GetValue<string>("AppSettings:audience"),
+                claims: claims,
+                expires:DateTime.UtcNow.AddHours(1),
+                signingCredentials: creds
+
+
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(toeknDescriptor);
+        }
+    }
+}
